abd63f887e106fa9c7de741a51058958876b7c6d
wc1217 <jxwangchao@gmail.com>
2022-01-09 03:16:45 +0800

    test ir nec and repeat code

--- pio/ir_nec/ir_loopback/ir_loopback.c
+++ pio/ir_nec/ir_loopback/ir_loopback.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <hardware/clocks.h>
 #include "pico/stdlib.h"
 
 #include "nec_transmit.h"                           // include the library headers
@@ -25,10 +26,10 @@ int main() {
     uint rx_gpio = 15;                              // choose which GPIO pin is connected to the IR detector
 
     // configure and enable the state machines
-    int tx_sm = nec_tx_init(pio, tx_gpio);         // uses two state machines, 16 instructions and one IRQ
+    // int tx_sm = nec_tx_init(pio, tx_gpio);         // uses two state machines, 16 instructions and one IRQ
     int rx_sm = nec_rx_init(pio, rx_gpio);         // uses one state machine and 9 instructions
 
-    if (tx_sm == -1 || rx_sm == -1) {
+    if (/*tx_sm == -1 || */rx_sm == -1) {
         printf("could not configure PIO\n");
         return -1;
     }
@@ -37,9 +38,9 @@ int main() {
     uint8_t tx_address = 0x00, tx_data = 0x00, rx_address, rx_data;
     while (true) {
         // create a 32-bit frame and add it to the transmit FIFO
-        uint32_t tx_frame = nec_encode_frame(tx_address, tx_data);
-        pio_sm_put(pio, tx_sm, tx_frame);
-        printf("\nsent: %02x, %02x", tx_address, tx_data);
+        // uint32_t tx_frame = nec_encode_frame(tx_address, tx_data);
+        // pio_sm_put(pio, tx_sm, tx_frame);
+        // printf("\nsent: %02x, %02x", tx_address, tx_data);
 
         // allow time for the frame to be transmitted (optional)
         sleep_ms(100);
@@ -49,13 +50,13 @@ int main() {
             uint32_t rx_frame = pio_sm_get(pio, rx_sm);
 
             if (nec_decode_frame(rx_frame, &rx_address, &rx_data)) {
-                printf("\treceived: %02x, %02x", rx_address, rx_data);
+                printf("\nreceived: %02x, %02x", rx_address, rx_data);
             } else {
-                printf("\treceived: %08x", rx_frame);
+                printf("\nreceived: %08x", rx_frame);
             }
         }
 
-        sleep_ms(900);
-        tx_data += 1;
+        // sleep_ms(900);
+        // tx_data += 1;
     }
 }
